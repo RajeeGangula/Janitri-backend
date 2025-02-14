fa990d9165ed8a85e517e58088215691591e06ae
Rajee Gangula <137306345+RajeeGangula@users.noreply.github.com>
2025-02-14 13:13:49 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,22 @@
+package com.janitri.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
