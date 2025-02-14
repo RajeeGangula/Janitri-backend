389a52bfb5ae5c7ea6737ccaf888ea7b23314160
Rajee Gangula <137306345+RajeeGangula@users.noreply.github.com>
2025-02-14 13:16:13 +0530

    HeartRate.java

---
+++ HeartRate.java
@@ -0,0 +1,25 @@
+package com.janitri.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "heart_rate")
+public class HeartRate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+
+    private int heartRate;
+    private LocalDateTime timestamp;
+}
