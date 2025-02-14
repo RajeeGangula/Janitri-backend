b47ac26b1e422ffe78a7f3e0a5b58052c740bc68
Rajee Gangula <137306345+RajeeGangula@users.noreply.github.com>
2025-02-14 13:15:08 +0530

    Patient.java

---
+++ Patient.java
@@ -0,0 +1,19 @@
+package com.janitri.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "patients")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int age;
+}
